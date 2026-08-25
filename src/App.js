b793a3122b51193c61c4dec3b9805310dbe02781
Prasad Nelaturi <prasadnelaturi333@gmail.com>
2026-08-25 13:27:53 +0530

    duplicate titles 1.1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { HelmetProvider } from 'react-helmet-async';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import SEO from './components/SEO';
-
 import ScrollToTop from './components/ScrollToTop';
 import Layout from './components/Layout';
 
@@ -44,7 +42,6 @@ const App = () => {
     <HelmetProvider>
       <ConsultationProvider>
         <BrowserRouter>
-          <SEO />
           <ScrollToTop />
           <ToastContainer position="top-center" pauseOnHover={false} />
 
